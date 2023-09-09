b9cb688f83e9e08a4816150fab0387d1b088181a
neonphog <neonphog@gmail.com>
2023-09-08 23:38:13 -0600

    checkpoint

--- crates/tx5-pipe/examples/textboard/main.rs
+++ crates/tx5-pipe/examples/textboard/main.rs
@@ -1,9 +1,16 @@
+use std::sync::{Arc, Mutex};
+
 mod state;
 use bytes::*;
 use state::*;
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
+    if !crossterm::tty::IsTty::is_tty(&std::io::stdout()) {
+        eprintln!("textboard is a command-line tty application, no piping");
+        std::process::exit(127);
+    }
+
     struct PHnd;
 
     impl tx5_pipe_control::Tx5PipeControlHandler for PHnd {
@@ -14,17 +21,45 @@ async fn main() {
 
     let pipe = tx5_pipe::Tx5Pipe::spawn_in_process(PHnd).await.unwrap();
 
+    /*
     let cli_url = pipe
         .sig_reg("wss://signal.holo.host".to_string())
         .await
         .unwrap();
+    */
+    let cli_url = "bla".to_string();
 
     struct SHnd {
         pipe: tx5_pipe_control::Tx5PipeControl,
+        board_task: Arc<
+            Mutex<
+                Option<
+                    tokio::task::JoinHandle<
+                        std::result::Result<
+                            std::io::Result<
+                                tokio::task::JoinHandle<
+                                    std::result::Result<
+                                        std::io::Result<()>,
+                                        tokio::task::JoinError,
+                                    >,
+                                >,
+                            >,
+                            tokio::task::JoinError,
+                        >,
+                    >,
+                >,
+            >,
+        >,
     }
 
+    let board_task = Arc::new(Mutex::new(None));
+
     impl StateHnd for SHnd {
-        fn prompt(&self, p: String) -> BoxFut<String> {
+        fn quit(&self) {
+            self.pipe.quit();
+        }
+
+        fn prompt(&self, _: &Arc<State>, p: String) -> BoxFut<String> {
             use std::io::Write;
             Box::pin(async move {
                 tokio::task::spawn_blocking(move || {
@@ -42,15 +77,212 @@ async fn main() {
             })
         }
 
-        fn show_board(&self) {
-            println!("show board");
-            std::process::exit(0);
+        fn show_board(&self, state: &Arc<State>) {
+            let state = state.clone();
+            *self.board_task.lock().unwrap() = Some(tokio::task::spawn(
+                tokio::task::spawn_blocking(move || run_board(state.clone())),
+            ));
         }
     }
 
-    let state = State::new(SHnd { pipe }, cli_url).await;
+    let state = State::new(
+        SHnd {
+            pipe,
+            board_task: board_task.clone(),
+        },
+        cli_url,
+    )
+    .await;
 
-    println!("yo: {state:?}");
+    while !state.should_exit() {
+        tokio::time::sleep(std::time::Duration::from_millis(17)).await;
+    }
 
-    state.wait_exit().await;
+    if let Some(board_task) = board_task.lock().unwrap().take() {
+        board_task
+            .await
+            .unwrap()
+            .unwrap()
+            .unwrap()
+            .await
+            .unwrap()
+            .unwrap()
+            .unwrap();
+    };
+}
+
+fn run_event(state: Arc<State>) -> std::io::Result<()> {
+    use crossterm::event::*;
+
+    loop {
+        if poll(std::time::Duration::from_millis(17))? {
+            match read()? {
+                Event::Key(event) => {
+                    if event.code == KeyCode::Esc
+                        || (event.code == KeyCode::Char('c')
+                            && event.modifiers.contains(KeyModifiers::CONTROL))
+                    {
+                        state.quit();
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        if state.should_exit() {
+            break;
+        }
+    }
+
+    Ok(())
+}
+
+fn run_board(
+    state: Arc<State>,
+) -> std::io::Result<
+    tokio::task::JoinHandle<
+        std::result::Result<std::io::Result<()>, tokio::task::JoinError>,
+    >,
+> {
+    use std::io::Write;
+
+    struct OnDrop;
+
+    impl Drop for OnDrop {
+        fn drop(&mut self) {
+            let mut stdout = std::io::stdout().lock();
+
+            let _ = crossterm::execute!(
+                stdout,
+                crossterm::cursor::Show,
+                crossterm::terminal::EnableLineWrap,
+                crossterm::terminal::LeaveAlternateScreen,
+            );
+
+            let _ = crossterm::terminal::disable_raw_mode();
+        }
+    }
+
+    let _on_drop = OnDrop;
+
+    {
+        let mut stdout = std::io::stdout().lock();
+
+        crossterm::terminal::enable_raw_mode()?;
+
+        crossterm::execute!(
+            stdout,
+            crossterm::terminal::EnterAlternateScreen,
+            //crossterm::terminal::DisableLineWrap,
+            crossterm::cursor::Hide,
+        )?;
+    }
+
+    let (width, height) = crossterm::terminal::size()?;
+
+    let mut offset_x: i32 = width as i32 / -2;
+    let mut offset_y: i32 = height as i32 / -2;
+
+    let event_state = state.clone();
+    let event_task =
+        tokio::task::spawn(tokio::task::spawn_blocking(move || {
+            run_event(event_state)
+        }));
+
+    let mut block = [[state::Node::default(); state::BLOCK]; state::BLOCK];
+
+    loop {
+        {
+            let (width, height) = crossterm::terminal::size()?;
+
+            let mut stdout = std::io::stdout().lock();
+
+            crossterm::queue!(
+                stdout,
+                crossterm::terminal::BeginSynchronizedUpdate,
+                crossterm::terminal::Clear(crossterm::terminal::ClearType::Purge),
+                crossterm::style::ResetColor,
+                crossterm::cursor::MoveTo(1, 1),
+            )?;
+
+            let mut cx = (offset_x / state::BLOCK_I32) * state::BLOCK_I32;
+            if cx > offset_x {
+                cx -= state::BLOCK_I32;
+            }
+
+            let mut cy = (offset_y / state::BLOCK_I32) * state::BLOCK_I32;
+            if cy > offset_y {
+                cy -= state::BLOCK_I32;
+            }
+
+            for x in (cx..offset_x + width as i32).step_by(state::BLOCK) {
+                for y in (cy..offset_y + height as i32).step_by(state::BLOCK) {
+                    /*
+                    crossterm::queue!(
+                        stdout,
+                        crossterm::style::Print(format!("(B{x},{y})")),
+                    )?;
+                    */
+
+                    state.fill_block(x, y, &mut block);
+
+                    for iy in 0..state::BLOCK_I32 {
+                        if iy - offset_y < 0 ||
+                            iy - offset_y > height as i32
+                        {
+                            continue;
+                        }
+                        let mut set_pos = false;
+                        for ix in 0..state::BLOCK_I32 {
+                            if ix - offset_x < 0 ||
+                                ix - offset_x > width as i32
+                            {
+                                continue;
+                            }
+                            if !set_pos {
+                                set_pos = true;
+                                crossterm::queue!(
+                                    stdout,
+                                    crossterm::cursor::MoveTo(ix as u16, iy as u16),
+                                )?;
+                            }
+                            crossterm::queue!(
+                                stdout,
+                                crossterm::style::Print(block[ix as usize][iy as usize].val),
+                            )?;
+                            /*
+                            crossterm::queue!(
+                                stdout,
+                                crossterm::style::Print(format!("(I{ix},{iy})")),
+                            )?;
+                            */
+                        }
+                    }
+                }
+            }
+
+            crossterm::queue!(
+                stdout,
+                crossterm::cursor::MoveTo(0, 0),
+                crossterm::style::SetForegroundColor(crossterm::style::Color::Black),
+                crossterm::style::SetBackgroundColor(crossterm::style::Color::White),
+                crossterm::style::Print("━".repeat(width as usize)),
+                crossterm::cursor::MoveTo(0, height - 1),
+                crossterm::style::Print("━".repeat(width as usize)),
+                crossterm::style::ResetColor,
+                crossterm::cursor::MoveTo(0, 0),
+                crossterm::terminal::EndSynchronizedUpdate,
+            )?;
+
+            stdout.flush()?;
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(17));
+
+        if state.should_exit() {
+            break;
+        }
+    }
+
+    Ok(event_task)
 }
--- crates/tx5-pipe/examples/textboard/state.rs
+++ crates/tx5-pipe/examples/textboard/state.rs
@@ -1,67 +1,167 @@
+use std::borrow::Cow;
 use std::collections::HashMap;
 use std::future::Future;
 use std::pin::Pin;
-use std::sync::{Arc, Mutex};
+use std::sync::{atomic, Arc, Mutex};
 
-pub type BoxFut<R> = Pin<Box<dyn Future<Output = R> + 'static + Send>>;
+/// The size of the text blocks
+pub const BLOCK_I32: i32 = 32;
+pub const BLOCK: usize = BLOCK_I32 as usize;
 
-pub trait StateHnd: 'static + Send + Sync {
-    fn prompt(&self, p: String) -> BoxFut<String>;
-    fn show_board(&self);
+enum Coms<'lt> {
+    /// "joined" peers send this notifying of updates they've made
+    /// and requesting blocks if they have changes > last recvd versions
+    ClientUpdate {
+        want_blocks: Vec<(i32, i32, i32)>, // x, y, ver
+        cur_pos: (i32, i32),               // x, y
+        updates: Vec<(i32, i32, Node)>,    // x, y, node
+    },
+
+    /// "host" peers send this info updating the clients about the state
+    ServerUpdate {
+        cursors: Vec<(i32, i32, String)>, // x, y, nickname
+        blocks: Vec<(
+            i32, // x
+            i32, // y
+            i32, // ver
+            Cow<'lt, [[Node; BLOCK]; BLOCK]>,
+        )>,
+    },
 }
 
-type DynStateHnd = Arc<dyn StateHnd + 'static + Send + Sync>;
-
-#[derive(Clone, Copy)]
-pub struct Node([u8; 8]);
-
-impl Default for Node {
-    fn default() -> Self {
-        // the additional bytes are reserved for style
-        // fg/bg color, etc
-        Self([32, 0, 0, 0, 0, 0, 0, 0])
+impl<'lt> Coms<'lt> {
+    pub fn encode(&self, out: &mut String) {
+        match self {
+            Self::ClientUpdate {
+                want_blocks,
+                cur_pos,
+                updates,
+            } => {
+                todo!()
+            }
+            Self::ServerUpdate { cursors, blocks } => {
+                out.push_str(&format!("S\0{}\0", cursors.len()));
+                for (x, y, nickname) in cursors {
+                    out.push_str(&format!("{x}\0{y}\0{nickname}\0"));
+                }
+                out.push_str(&format!("{}\0", blocks.len()));
+                for (x, y, ver, block) in blocks {
+                    out.push_str(&format!("{x}\0{y}\0{ver}\0"));
+                    for x in 0..BLOCK {
+                        for y in 0..BLOCK {
+                            block[x][y].encode(out);
+                        }
+                    }
+                    out.push('\0');
+                }
+            }
+        }
     }
-}
 
-impl std::fmt::Debug for Node {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.get_char().fmt(f)
-    }
-}
-
-impl Node {
-    /*
-    #[inline(always)]
-    pub fn set_char(&mut self, c: char) {
-        self.0[0..4].copy_from_slice(&(c as u32).to_le_bytes());
-    }
-
-    #[inline(always)]
-    pub fn with_char(mut self, c: char) -> Self {
-        self.set_char(c);
-        self
-    }
-    */
-
-    #[inline(always)]
-    pub fn get_char(&self) -> char {
-        match char::from_u32(u32::from_le_bytes([
-            self.0[0], self.0[1], self.0[2], self.0[3],
-        ])) {
-            Some(c) => c,
-            None => ' ',
+    pub fn decode(&self, input: &str) -> Self {
+        let mut input = input.split('\0');
+        match input.next().unwrap() {
+            "C" => {
+                todo!()
+            }
+            "S" => {
+                let cursor_cnt: usize = input.next().unwrap().parse().unwrap();
+                let mut cursors = Vec::new();
+                for _ in 0..cursor_cnt {
+                    let x: i32 = input.next().unwrap().parse().unwrap();
+                    let y: i32 = input.next().unwrap().parse().unwrap();
+                    let nickname = input.next().unwrap().to_string();
+                    cursors.push((x, y, nickname));
+                }
+                let block_cnt: usize = input.next().unwrap().parse().unwrap();
+                let mut blocks = Vec::new();
+                for _ in 0..block_cnt {
+                    let x: i32 = input.next().unwrap().parse().unwrap();
+                    let y: i32 = input.next().unwrap().parse().unwrap();
+                    let ver: i32 = input.next().unwrap().parse().unwrap();
+                    let mut out = [[Node::default(); BLOCK]; BLOCK];
+                    let mut block = input.next().unwrap().chars();
+                    for x in 0..BLOCK {
+                        for y in 0..BLOCK {
+                            let fg = block.next().unwrap();
+                            let bg = block.next().unwrap();
+                            let val = block.next().unwrap();
+                            out[x][y] = Node {
+                                fg: fg as u32 as u8,
+                                bg: bg as u32 as u8,
+                                val,
+                            };
+                        }
+                    }
+                    blocks.push((x, y, ver, Cow::Owned(out)));
+                }
+                Self::ServerUpdate { cursors, blocks }
+            }
+            _ => panic!(),
         }
     }
 }
 
+pub type BoxFut<R> = Pin<Box<dyn Future<Output = R> + 'static + Send>>;
+
+pub trait StateHnd: 'static + Send + Sync {
+    fn quit(&self);
+    fn prompt(&self, state: &Arc<State>, p: String) -> BoxFut<String>;
+    fn show_board(&self, state: &Arc<State>);
+}
+
+type DynStateHnd = Arc<dyn StateHnd + 'static + Send + Sync>;
+
+pub const BLACK: char = '0';
+pub const DARK_RED: char = '1';
+pub const DARK_GREEN: char = '2';
+pub const DARK_YELLOW: char = '3';
+pub const DARK_BLUE: char = '4';
+pub const DARK_MAGENTA: char = '5';
+pub const DARK_CYAN: char = '6';
+pub const DARK_GREY: char = '7';
+pub const GREY: char = '8';
+pub const RED: char = '9';
+pub const GREEN: char = 'a';
+pub const YELLOW: char = 'b';
+pub const BLUE: char = 'c';
+pub const MAGENTA: char = 'd';
+pub const CYAN: char = 'e';
+pub const WHITE: char = 'f';
+
+#[derive(Debug, Clone, Copy)]
+pub struct Node {
+    pub fg: u8,
+    pub bg: u8,
+    pub val: char,
+}
+
+impl Default for Node {
+    fn default() -> Self {
+        Self {
+            fg: WHITE as u32 as u8,
+            bg: BLACK as u32 as u8,
+            val: ' ',
+        }
+    }
+}
+
+impl Node {
+    pub fn encode(&self, out: &mut String) {
+        out.push(char::from_u32(self.fg as u32).unwrap());
+        out.push(char::from_u32(self.bg as u32).unwrap());
+        out.push(self.val);
+    }
+}
+
 pub struct State {
+    exit: atomic::AtomicBool,
     hnd: DynStateHnd,
     cli_url: String,
     nick: Mutex<String>,
     rem_host: Mutex<Option<String>>,
-    exit: Arc<tokio::sync::Notify>,
-    blocks: Mutex<HashMap<(i32, i32), Arc<Mutex<[[Node; 64]; 64]>>>>,
-    overlay: Mutex<HashMap<(i32, i32), (Node, std::time::Instant)>>,
+    blocks: Mutex<HashMap<(i32, i32), Arc<Mutex<[[Node; BLOCK]; BLOCK]>>>>,
+    overlay: Mutex<HashMap<(i32, i32), (Node, i32)>>,
 }
 
 impl std::fmt::Debug for State {
@@ -78,16 +178,16 @@ impl State {
         let hnd: DynStateHnd = Arc::new(hnd);
 
         let this = Arc::new(Self {
+            exit: atomic::AtomicBool::new(false),
             hnd,
             cli_url,
             nick: Mutex::new("noname".to_string()),
             rem_host: Mutex::new(None),
-            exit: Arc::new(tokio::sync::Notify::new()),
             blocks: Mutex::new(HashMap::new()),
             overlay: Mutex::new(HashMap::new()),
         });
 
-        this.prompt_nick();
+        this.clone().prompt_nick();
 
         this
     }
@@ -96,13 +196,13 @@ impl State {
         &self,
         offset_x: i32,
         offset_y: i32,
-        block: &mut [[Node; 64]; 64],
+        block: &mut [[Node; BLOCK]; BLOCK],
     ) {
-        if (offset_x / 64) * 64 != offset_x {
-            panic!("invalid offset_x not divisible by 64");
+        if (offset_x / BLOCK_I32) * BLOCK_I32 != offset_x {
+            panic!("invalid offset_x not divisible by block size");
         }
-        if (offset_y / 64) * 64 != offset_y {
-            panic!("invalid offset_y not divisible by 64");
+        if (offset_y / BLOCK_I32) * BLOCK_I32 != offset_y {
+            panic!("invalid offset_y not divisible by BLOCK");
         }
         {
             let sblock = self
@@ -111,37 +211,47 @@ impl State {
                 .unwrap()
                 .entry((offset_x, offset_y))
                 .or_insert_with(|| {
-                    Arc::new(Mutex::new([[Node::default(); 64]; 64]))
+                    Arc::new(Mutex::new([[Node::default(); BLOCK]; BLOCK]))
                 })
                 .clone();
             *block = *sblock.lock().unwrap();
         }
         let overlay_lock = self.overlay.lock().unwrap();
-        for x in 0..64 {
-            for y in 0..64 {
-                if let Some((n, _)) = overlay_lock.get(&(x + offset_x, y + offset_y)) {
+        for x in 0..BLOCK_I32 {
+            for y in 0..BLOCK_I32 {
+                if let Some((n, _)) =
+                    overlay_lock.get(&(x + offset_x, y + offset_y))
+                {
                     block[x as usize][y as usize] = *n;
+                }
+                if x == 0 || y == 0 {
+                    block[x as usize][y as usize].val = '.';
                 }
             }
         }
     }
 
-    pub async fn wait_exit(&self) {
-        self.exit.notified().await;
+    pub fn should_exit(&self) -> bool {
+        self.exit.load(atomic::Ordering::Relaxed)
     }
 
-    fn prompt_nick(self: &Arc<Self>) {
-        let this = self.clone();
+    pub fn quit(&self) {
+        self.exit.store(true, atomic::Ordering::Relaxed);
+        self.hnd.quit();
+    }
+
+    // -- private -- //
+
+    fn prompt_nick(self: Arc<Self>) {
         tokio::task::spawn(async move {
-            let nick = this.hnd.prompt("nickname> ".to_string()).await;
-            *this.nick.lock().unwrap() = nick;
-            println!("nick: {}", this.nick.lock().unwrap());
-            this.prompt_join_type();
+            let nick = self.hnd.prompt(&self, "nickname> ".to_string()).await;
+            *self.nick.lock().unwrap() = nick;
+            println!("nick: {}", self.nick.lock().unwrap());
+            self.prompt_join_type();
         });
     }
 
-    fn prompt_join_type(self: &Arc<Self>) {
-        let this = self.clone();
+    fn prompt_join_type(self: Arc<Self>) {
         tokio::task::spawn(async move {
             let prompt = format!(
                 r#"-- Your Client URL if hosting --
@@ -149,35 +259,40 @@ impl State {
 1) Host Private TextBoard
 2) Join Private TextBoard
 join type> "#,
-                this.cli_url,
+                self.cli_url,
             );
-            let jt = this.hnd.prompt(prompt).await;
+            let jt = self.hnd.prompt(&self, prompt).await;
 
             match jt.as_str() {
                 "1" => {
                     println!("join type: priv host");
-                    this.hnd.show_board();
+                    self.start_board();
                 }
                 "2" => {
                     println!("join type: priv join");
-                    this.prompt_join_priv_rem_url();
+                    self.prompt_join_priv_rem_url();
                 }
                 _ => {
                     eprintln!("invalid join type: {jt}");
-                    this.prompt_join_type();
+                    self.prompt_join_type();
                 }
             }
         });
     }
 
-    fn prompt_join_priv_rem_url(self: &Arc<Self>) {
-        let this = self.clone();
+    fn prompt_join_priv_rem_url(self: Arc<Self>) {
         tokio::task::spawn(async move {
-            let rem_url =
-                this.hnd.prompt("remote client url> ".to_string()).await;
+            let rem_url = self
+                .hnd
+                .prompt(&self, "remote client url> ".to_string())
+                .await;
             println!("rem_url: {rem_url}");
-            *this.rem_host.lock().unwrap() = Some(rem_url);
-            this.hnd.show_board();
+            *self.rem_host.lock().unwrap() = Some(rem_url);
+            self.start_board();
         });
+    }
+
+    fn start_board(self: Arc<Self>) {
+        self.hnd.show_board(&self);
     }
 }

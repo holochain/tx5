6f2e9d4f413b96e1fff0807288e8bc9ad6411fa8
neonphog <neonphog@gmail.com>
2024-01-08 11:07:38 -0700

    enable android 33 test

--- android-run-tests.bash
+++ android-run-tests.bash
@@ -2,7 +2,15 @@
 
 set -eEuxo pipefail
 
+trap 'cleanup' ERR EXIT
+cleanup() {
+  for i in $(cat output-test-executables); do
+    adb shell rm -f /data/local/tmp/$(basename $i)
+  done
+}
+
 for i in $(cat output-test-executables); do
   adb push $i /data/local/tmp/$(basename $i)
   adb shell RUST_BACKTRACE=1 /data/local/tmp/$(basename $i) --test-threads 1 --nocapture
+  adb shell rm -f /data/local/tmp/$(basename $i)
 done

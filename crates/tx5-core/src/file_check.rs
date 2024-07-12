8a93170146247fa3f0a01e8ab2ebb8c930145410
Matt Gabrenya <matt@buildyourweb.app>
2024-07-12 14:00:45 -0700

    Backport of https://github.com/holochain/tx5/pull/97 for holochain 0.3

--- crates/tx5-core/src/file_check.rs
+++ crates/tx5-core/src/file_check.rs
@@ -2,6 +2,7 @@
 //! files on system. Enable this `file_check` feature to provide that ability.
 
 use crate::{Error, Result};
+use std::path::PathBuf;
 
 /// A handle to a verified system file. Keep this instance in memory as
 /// long as you intend to keep using the validated file.
@@ -17,20 +18,32 @@ impl FileCheck {
     }
 }
 
-fn get_user_cache_dir() -> Option<std::path::PathBuf> {
-    match app_dirs2::app_root(
-        app_dirs2::AppDataType::UserCache,
-        &app_dirs2::AppInfo {
-            name: "host.holo.tx5",
-            author: "host.holo.tx5",
-        },
-    ) {
-        Ok(dir) => Some(dir),
-        _ => None,
+/// Get a path to a cache directory where the dependency Pion library can be written
+///
+/// Defaults to the UserCache directory returned by app_dirs2, specific to the target platform.
+/// Overridable via the env variable TX5_CACHE_DIRECTORY
+fn get_cache_dir() -> Result<std::path::PathBuf> {
+    match std::env::var("TX5_CACHE_DIRECTORY") {
+        Ok(cache_dir) => {
+            let path = PathBuf::from(cache_dir);
+            if path.is_dir() {
+                Ok(path)
+            } else {
+                Err(std::io::Error::other("env variable TX5_CACHE_DIRECTORY is set, but it is not a valid path to an existing directory"))
+            }
+        }
+        Err(_) => app_dirs2::app_root(
+            app_dirs2::AppDataType::UserCache,
+            &app_dirs2::AppInfo {
+                name: "host.holo.tx5",
+                author: "host.holo.tx5",
+            },
+        )
+        .map_err(std::io::Error::other),
     }
 }
 
-/// Write a file if needed, verify the file, and return a handle to that file.
+/// Write a temp file if needed, verify the file, and return a handle to that file.
 pub fn file_check(
     file_data: &[u8],
     file_hash: &str,
@@ -38,57 +51,52 @@ pub fn file_check(
     file_name_ext: &str,
 ) -> Result<FileCheck> {
     let file_name = format!("{file_name_prefix}-{file_hash}{file_name_ext}");
+    let tmp_dir = get_cache_dir()?;
 
-    let pref_path = get_user_cache_dir().map(|mut d| {
-        d.push(&file_name);
-        d
-    });
+    let mut pref_path = tmp_dir.clone();
+    pref_path.push(&file_name);
 
-    if let Some(pref_path) = pref_path.as_ref() {
-        if let Ok(file) = validate(pref_path, file_hash) {
+    if let Ok(file) = validate(&pref_path, file_hash) {
+        return Ok(FileCheck {
+            path: pref_path.clone(),
+            _file: Some(file),
+        });
+    }
+
+    let mut tmp = write(tmp_dir, file_data)?;
+
+    // NOTE: This is NOT atomic, nor secure, but being able to validate the
+    //       file hash post-op mitigates this a bit. And we can let the os
+    //       clean up a dangling tmp file if it failed to unlink.
+    match tmp.persist_noclobber(pref_path.clone()) {
+        Ok(mut file) => {
+            set_perms(&mut file)?;
+
+            drop(file);
+
+            let file = validate(&pref_path, file_hash)?;
+
             return Ok(FileCheck {
                 path: pref_path.clone(),
                 _file: Some(file),
             });
+        }
+        Err(err) => {
+            let tempfile::PersistError { file, .. } = err;
+            tmp = file;
         }
     }
 
-    let mut tmp = write(file_data)?;
+    // before we go on to just using the tmp file,
+    // check to see if a different process wrote correctly
+    if let Ok(file) = validate(&pref_path, file_hash) {
+        // we no longer need the tmp file, clean it up
+        let _ = tmp.close();
 
-    if let Some(pref_path) = pref_path.as_ref() {
-        // NOTE: This is NOT atomic, nor secure, but being able to validate the
-        //       file hash post-op mitigates this a bit. And we can let the os
-        //       clean up a dangling tmp file if it failed to unlink.
-        match tmp.persist_noclobber(pref_path) {
-            Ok(mut file) => {
-                set_perms(&mut file)?;
-
-                drop(file);
-
-                let file = validate(pref_path, file_hash)?;
-
-                return Ok(FileCheck {
-                    path: pref_path.clone(),
-                    _file: Some(file),
-                });
-            }
-            Err(err) => {
-                let tempfile::PersistError { file, .. } = err;
-                tmp = file;
-            }
-        }
-
-        // before we go on to just using the tmp file,
-        // check to see if a different process wrote correctly
-        if let Ok(file) = validate(pref_path, file_hash) {
-            // we no longer need the tmp file, clean it up
-            let _ = tmp.close();
-
-            return Ok(FileCheck {
-                path: pref_path.clone(),
-                _file: Some(file),
-            });
-        }
+        return Ok(FileCheck {
+            path: pref_path.clone(),
+            _file: Some(file),
+        });
     }
 
     // we're just going to use the tmp file, do what we need to
@@ -143,11 +151,14 @@ fn validate(path: &std::path::PathBuf, hash: &str) -> Result<std::fs::File> {
     Ok(file)
 }
 
-/// Write a temp file.
-fn write(file_data: &[u8]) -> Result<tempfile::NamedTempFile> {
+/// Write a temp file in the given directory
+fn write(
+    parent_dir: PathBuf,
+    file_data: &[u8],
+) -> Result<tempfile::NamedTempFile> {
     use std::io::Write;
 
-    let mut tmp = tempfile::NamedTempFile::new()?;
+    let mut tmp = tempfile::NamedTempFile::new_in(parent_dir)?;
 
     tmp.as_file_mut().write_all(file_data)?;
     tmp.as_file_mut().flush()?;
@@ -220,5 +231,48 @@ mod tests {
             drop(tmp);
             let _ = std::fs::remove_file(&path);
         }
+    }
+
+    #[test]
+    fn file_check_env_variable_override() {
+        let _ = tempfile::tempdir().unwrap();
+        let tmpdir = tempfile::tempdir().unwrap();
+        let tmpdir_path = tmpdir.path();
+        let original_tx5_cache_directory = std::env::var("TX5_CACHE_DIRECTORY");
+        std::env::set_var("TX5_CACHE_DIRECTORY", tmpdir_path.as_os_str());
+
+        use rand::Rng;
+        let mut data = vec![0; 1024 * 1024 * 10]; // 10 MiB
+        rand::thread_rng().fill(&mut data[..]);
+        let data = Arc::new(data);
+
+        use sha2::Digest;
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(&data[..]);
+
+        let hash = 
+            base64::encode_config(hasher.finalize(), base64::URL_SAFE_NO_PAD);
+
+        let data = data.clone();
+        let hash = hash.clone();
+
+        let res = file_check(
+            data.as_slice(),
+            &hash,
+            "tx5-core-file-check-test",
+            ".data",
+        )
+        .unwrap();
+
+        assert!(res.path.starts_with(tmpdir_path));
+
+        // cleanup
+        let path = res.path().to_owned();
+        match original_tx5_cache_directory {
+            Ok(dir) => std::env::set_var("TX5_CACHE_DIRECTORY", dir),
+            Err(_) => std::env::remove_var("TX5_CACHE_DIRECTORY"),
+        };
+        drop(res);
+        let _ = std::fs::remove_file(path);
     }
 }

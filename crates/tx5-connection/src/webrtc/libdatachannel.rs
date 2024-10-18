0cc03872ca586e95a848675163d7aba298714bd4
neonphog <neonphog@gmail.com>
2024-10-18 14:18:11 -0600

    integration complete, 1 failing test

--- crates/tx5-connection/src/webrtc/libdatachannel.rs
+++ crates/tx5-connection/src/webrtc/libdatachannel.rs
@@ -2,47 +2,50 @@ use super::*;
 use crate::{AbortTask, CloseRecv, CloseSend};
 use std::io::{Error, Result};
 
+type MapErr<E, F> = Box<dyn FnOnce(E) -> F>;
+fn map_err<E: std::fmt::Debug>(s: &'static str) -> MapErr<E, std::io::Error> {
+    Box::new(move |e| std::io::Error::other(format!("{s}: {e:?}")))
+}
+
 enum Cmd {
     InOffer(Vec<u8>),
     InAnswer(Vec<u8>),
     InIce(Vec<u8>),
     GeneratedIce(datachannel::IceCandidate),
-    DataChan(Box<datachannel::RtcDataChannel<DCH>>),
+    DataChan(Box<datachannel::RtcDataChannel<Dch>>),
     SendMessage(Vec<u8>, tokio::sync::oneshot::Sender<()>),
     RecvMessage(Vec<u8>),
-    RecvDescription(datachannel::SessionDescription),
+    RecvDescription(Box<datachannel::SessionDescription>),
     DataChanOpen,
     BufferedAmountLow,
     Error(std::io::Error),
 }
 
-struct DCH(CloseSend<Cmd>);
+struct Dch(CloseSend<Cmd>);
 
-impl datachannel::DataChannelHandler for DCH {
+impl datachannel::DataChannelHandler for Dch {
     fn on_open(&mut self) {
-        let _ = self.0.send_or_close(Cmd::DataChanOpen);
+        self.0.send_or_close(Cmd::DataChanOpen);
     }
 
     fn on_closed(&mut self) {
-        let _ = self
-            .0
+        self.0
             .send_or_close(Cmd::Error(std::io::Error::other("DataChanClosed")));
     }
 
     fn on_error(&mut self, err: &str) {
-        let _ =
-            self.0
-                .send_or_close(Cmd::Error(std::io::Error::other(format!(
-                    "DataChanError: {err}"
-                ))));
+        self.0
+            .send_or_close(Cmd::Error(std::io::Error::other(format!(
+                "DataChanError: {err}"
+            ))));
     }
 
     fn on_message(&mut self, msg: &[u8]) {
-        let _ = self.0.send_or_close(Cmd::RecvMessage(msg.to_vec()));
+        self.0.send_or_close(Cmd::RecvMessage(msg.to_vec()));
     }
 
     fn on_buffered_amount_low(&mut self) {
-        let _ = self.0.send_or_close(Cmd::BufferedAmountLow);
+        self.0.send_or_close(Cmd::BufferedAmountLow);
     }
 
     /*
@@ -52,31 +55,32 @@ impl datachannel::DataChannelHandler for DCH {
     */
 }
 
-struct PCH(CloseSend<Cmd>);
+struct Pch(CloseSend<Cmd>);
 
-impl datachannel::PeerConnectionHandler for PCH {
-    type DCH = DCH;
+impl datachannel::PeerConnectionHandler for Pch {
+    type DCH = Dch;
 
     fn data_channel_handler(
         &mut self,
         _info: datachannel::DataChannelInfo,
     ) -> Self::DCH {
-        DCH(self.0.clone())
+        Dch(self.0.clone())
     }
 
     fn on_description(&mut self, sess_desc: datachannel::SessionDescription) {
-        let _ = self.0.send_or_close(Cmd::RecvDescription(sess_desc));
+        self.0
+            .send_or_close(Cmd::RecvDescription(Box::new(sess_desc)));
     }
 
     fn on_candidate(&mut self, cand: datachannel::IceCandidate) {
-        let _ = self.0.send_or_close(Cmd::GeneratedIce(cand));
+        self.0.send_or_close(Cmd::GeneratedIce(cand));
     }
 
     fn on_data_channel(
         &mut self,
         data_channel: Box<datachannel::RtcDataChannel<Self::DCH>>,
     ) {
-        let _ = self.0.send_or_close(Cmd::DataChan(data_channel));
+        self.0.send_or_close(Cmd::DataChan(data_channel));
     }
 }
 
@@ -87,11 +91,44 @@ pub struct Webrtc {
 }
 
 impl Webrtc {
+    #[allow(clippy::new_ret_no_self)]
+    #[allow(clippy::needless_return)]
     pub fn new(
         is_polite: bool,
         config: Vec<u8>,
         send_buffer: usize,
     ) -> (DynWebrtc, CloseRecv<WebrtcEvt>) {
+        static INIT_TRACING: std::sync::Once = std::sync::Once::new();
+        INIT_TRACING.call_once(|| {
+            use tracing::event_enabled;
+            use tracing::Level;
+
+            if !tx5_core::Tx5InitConfig::get().tracing_enabled {
+                return;
+            }
+
+            if event_enabled!(target: "datachannel", Level::TRACE) {
+                datachannel::configure_logging(Level::TRACE);
+                return;
+            }
+            if event_enabled!(target: "datachannel", Level::DEBUG) {
+                datachannel::configure_logging(Level::DEBUG);
+                return;
+            }
+            if event_enabled!(target: "datachannel", Level::INFO) {
+                datachannel::configure_logging(Level::INFO);
+                return;
+            }
+            if event_enabled!(target: "datachannel", Level::WARN) {
+                datachannel::configure_logging(Level::WARN);
+                return;
+            }
+            if event_enabled!(target: "datachannel", Level::ERROR) {
+                datachannel::configure_logging(Level::ERROR);
+                return;
+            }
+        });
+
         let (mut cmd_send, cmd_recv) = CloseSend::sized_channel(1024);
         let (mut evt_send, evt_recv) = CloseSend::sized_channel(1024);
 
@@ -166,14 +203,17 @@ async fn task(
     cmd_send: CloseSend<Cmd>,
     cmd_recv: CloseRecv<Cmd>,
 ) {
-    if let Err(err) = task_err(is_polite, config, send_buffer, evt_send, cmd_send, cmd_recv).await {
+    if let Err(err) =
+        task_err(is_polite, config, send_buffer, evt_send, cmd_send, cmd_recv)
+            .await
+    {
         tracing::debug!(?err, "webrtc task error");
     }
 }
 
 async fn task_err(
     is_polite: bool,
-    _config: Vec<u8>,
+    config: Vec<u8>,
     send_buffer: usize,
     mut evt_send: CloseSend<WebrtcEvt>,
     cmd_send: CloseSend<Cmd>,
@@ -181,13 +221,38 @@ async fn task_err(
 ) -> Result<()> {
     evt_send.set_close_on_drop(true);
 
-    // TODO - use actual ice servers
-    // TODO - max_message_size
-    // TODO - port range begin/end
-    let config = datachannel::RtcConfig::new::<String>(&[]);
+    #[derive(Debug, Default, serde::Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    struct U {
+        pub urls: Vec<String>,
+    }
+
+    #[derive(Debug, serde::Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    struct C {
+        #[serde(default)]
+        pub ice_servers: Vec<U>,
+    }
+
+    let mut ice = Vec::new();
+    match serde_json::from_slice::<C>(&config) {
+        Ok(mut c) => {
+            for mut u in c.ice_servers.drain(..) {
+                ice.append(&mut u.urls);
+            }
+        }
+        Err(err) => tracing::error!(?err, "failed to parse iceServers"),
+    }
+
+    let init_config = tx5_core::Tx5InitConfig::get();
+
+    // TODO - max_message_size?
+    let config = datachannel::RtcConfig::new::<String>(&ice)
+        .port_range_begin(init_config.ephemeral_udp_port_min)
+        .port_range_end(init_config.ephemeral_udp_port_max);
     let mut peer =
-        datachannel::RtcPeerConnection::new(&config, PCH(cmd_send.clone()))
-            .map_err(std::io::Error::other)?;
+        datachannel::RtcPeerConnection::new(&config, Pch(cmd_send.clone()))
+            .map_err(map_err("constructing peer connection"))?;
 
     let mut data = None;
     let mut did_handshake = false;
@@ -195,13 +260,13 @@ async fn task_err(
 
     if !is_polite {
         let mut d = peer
-            .create_data_channel("data", DCH(cmd_send.clone()))
-            .map_err(std::io::Error::other)?;
+            .create_data_channel("data", Dch(cmd_send.clone()))
+            .map_err(map_err("creating data channel"))?;
         d.set_buffered_amount_low_threshold(send_buffer)
-            .map_err(std::io::Error::other)?;
+            .map_err(map_err("setting buffer low threshold (out)"))?;
         data = Some(d);
         peer.set_local_description(datachannel::SdpType::Offer)
-            .map_err(std::io::Error::other)?;
+            .map_err(map_err("setting local desc to offer"))?;
     }
 
     loop {
@@ -218,11 +283,13 @@ async fn task_err(
                 if is_polite && !did_handshake {
                     let o: datachannel::SessionDescription =
                         serde_json::from_slice(&o)
-                            .map_err(std::io::Error::other)?;
+                            .map_err(map_err("deserializing remote offer"))?;
                     peer.set_remote_description(&o)
-                        .map_err(std::io::Error::other)?;
-                    peer.set_local_description(datachannel::SdpType::Answer)
-                        .map_err(std::io::Error::other)?;
+                        .map_err(map_err("setting remote offer desc"))?;
+                    // NOTE: I guess this auto-answers??
+                    // We get a Runtime error if we call this explicitly:
+                    //peer.set_local_description(datachannel::SdpType::Answer)
+                    //    .map_err(map_err("setting local desc to answer"))?;
                     did_handshake = true;
                 }
             }
@@ -231,9 +298,9 @@ async fn task_err(
                 if !is_polite && !did_handshake {
                     let a: datachannel::SessionDescription =
                         serde_json::from_slice(&a)
-                            .map_err(std::io::Error::other)?;
+                            .map_err(map_err("deserializing remote answer"))?;
                     peer.set_remote_description(&a)
-                        .map_err(std::io::Error::other)?;
+                        .map_err(map_err("setting remote answer desc"))?;
                     did_handshake = true;
                 }
             }
@@ -241,10 +308,15 @@ async fn task_err(
                 slow_task = "in-ice";
                 let i: datachannel::IceCandidate =
                     serde_json::from_slice(&i)
-                        .map_err(std::io::Error::other)?;
-                let _ = peer
+                        .map_err(map_err("deserializing remote candidate"))?;
+                if let Err(err) = peer
                     .add_remote_candidate(&i)
-                    .map_err(std::io::Error::other)?;
+                    .map_err(map_err("adding remote candidate"))
+                {
+                    // Don't error on ice candidates, it might be from
+                    // a previous negotiation, just note it in the trace
+                    tracing::debug!(?err, "failed to add remote candidate");
+                }
             }
             Cmd::GeneratedIce(ice) => {
                 slow_task = "gen-ice";
@@ -257,8 +329,9 @@ async fn task_err(
             Cmd::DataChan(mut d) => {
                 slow_task = "data-chan";
                 if data.is_none() {
-                    d.set_buffered_amount_low_threshold(send_buffer)
-                        .map_err(std::io::Error::other)?;
+                    d.set_buffered_amount_low_threshold(send_buffer).map_err(
+                        map_err("setting buffer low threshold (in)"),
+                    )?;
                     data = Some(d);
                 } else {
                     return Err(std::io::Error::other("duplicate data chan"));
@@ -267,7 +340,7 @@ async fn task_err(
             Cmd::SendMessage(msg, resp) => {
                 slow_task = "send-msg";
                 if let Some(d) = &mut data {
-                    d.send(&msg).map_err(std::io::Error::other)?;
+                    d.send(&msg).map_err(map_err("sending message"))?;
                     let amt = d.buffered_amount();
                     if amt <= send_buffer {
                         drop(resp);

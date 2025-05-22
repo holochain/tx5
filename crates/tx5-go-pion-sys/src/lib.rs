af5ca8d217c0e80144f4f44fb9d95ebb0f7bfe71
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-05-22 19:36:17 +0100

    ci: Add ci_pass check (#148)
    * ci: Add ci_pass check
    
    * chore: fix duplicated attribute warning

--- crates/tx5-go-pion-sys/src/lib.rs
+++ crates/tx5-go-pion-sys/src/lib.rs
@@ -18,10 +18,12 @@
     any(target_os = "macos", target_os = "ios", target_os = "tvos"),
     link(name = "CoreFoundation", kind = "framework")
 )]
+extern "C" {}
 #[cfg_attr(
     any(target_os = "macos", target_os = "ios", target_os = "tvos"),
     link(name = "Security", kind = "framework")
 )]
+extern "C" {}
 #[cfg_attr(
     any(target_os = "macos", target_os = "ios", target_os = "tvos"),
     link(name = "resolv")
